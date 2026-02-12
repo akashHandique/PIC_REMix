4914470d0fd5f4a307519b15e968475a3adb1f26
Akash Jyoti Handique <ajh287@uclive.ac.nz>
2026-02-13 11:36:30 +1300

    testing

---
+++ Process/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+"gmsgennt.cmd" "C:\Local\remix-pic\REMix-Pacific_Island_Countries\Process\225a\" "C:\Local\remix-pic\REMix-Pacific_Island_Countries\Process\" %1 "C:\Local\remix-pic\REMix-Pacific_Island_Countries\Process\225a\gamscntr.dat" "C:\Local\GAMS\43\" "CPLEX"
